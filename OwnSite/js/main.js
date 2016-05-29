8bf9b263f57c87ae656d4a643eb4d1cbc376f3de
szmate <larde15@gmail.com>
2016-05-29 13:07:07 +0200

    refactored javascript

--- OwnSite/js/main.js
+++ OwnSite/js/main.js
@@ -1,33 +1,31 @@
- validateContactData(){
-	var nameValue = document.getElementById("name").value;
-	var mailValue = document.getElementById("mail").value;
-	var commentValue = document.getElementById("comment").value;
+(function () {
+	document.getElementById("sendButton").addEventListener("click", sendMessage);
 	
-	if(nameValue.length < 3 && nameValue.length > 30 && nameValue.indexOf(" ") == -1){
-		alert( "Name invalid!" );
-        document.contact.name.focus();
-        return false;
-	}
-	
-	if(!mailValue.endsWith('.hu') && !mailValue.endsWith('.com')){
-		alert( "Email invalid!" );
-        document.contact.mail.focus();
-        return false;
-	}
-	
-	if(commentValue.length < 20 && commentValue > 500){
-		alert( "Comment invalid!" );
-        document.contact.comment.focus();
-        return false;
-	}
-	return( true );
- }
- 
- sendMessage(){
-	var nameValue = document.getElementById("name").value;
-	var mailValue = document.getElementById("mail").value;
-	var commentValue = document.getElementById("comment").value;
-	
-	alert( "{0}{1}{2}", nameValue,mailValue,commentValue );
-	alert( "Your Message was sent" );
- }
+    function validateContactData() {
+		var nameValue = document.getElementById("name").value;
+		var mailValue = document.getElementById("mail").value;
+		var commentValue = document.getElementById("comment").value;
+		
+		if(nameValue.length < 3){
+			return false;
+		}
+		else if(!mailValue.endsWith('.com')){
+			return false;
+		}
+		else if(comment.length < 20){
+			return false;
+		}
+		else{
+			return true;
+		}
+    };
+
+    function sendMessage() { 
+		if (validateContactData()) {
+			alert("Your Message was sent.");
+		}else {
+			alert("Invalid input.")
+		}
+    };
+
+})();
